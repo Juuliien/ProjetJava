4c4247318c390f2cf87959ae4379ca7babaa09e2
Juuliien <36441596+Juuliien@users.noreply.github.com>
2019-06-04 17:50:36 +0200

    Add files via upload

---
+++ ProjetJava3/src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import javax.swing.SwingUtilities;
+
+import gui.MainFrameCo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+
+			public void run() {
+				new MainFrameCo();
+			}
+		});
+	}
+
+}
