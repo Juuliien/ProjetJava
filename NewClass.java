ddf8f2eada149137c776a0c4908118d5b68255c9
Juuliien <36441596+Juuliien@users.noreply.github.com>
2019-06-08 23:30:59 +0200

    Add files via upload

---
+++ NewClass.java
@@ -0,0 +1,19 @@
+package javaapplication1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author chris
+ */
+public class NewClass {
+    
+    private int x ;
+    public NewClass(){x=1;} 
+    public int getx(){return this.x;}
+    
+}
